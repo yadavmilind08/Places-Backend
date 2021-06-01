471a9ec5db343a1da5584abad2c743063a7441be
Milind Yadav <yadavmilind08@gmail.com>
2021-06-01 14:02:35 +0530

    minor fixes

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -26,17 +26,17 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
-      new HttpError("Could not find a place for provided user id.", 404)
+      new HttpError("Could not find a places for provided user id.", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -78,7 +78,7 @@ const deletePlaceById = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlaceById = updatePlaceById;
 exports.deletePlaceById = deletePlaceById;
--- routes/places-route.js
+++ routes/places-route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/:pid", placesController.getPlaceById);
 
-router.get("/user/:uid", placesController.getPlaceByUserId);
+router.get("/user/:uid", placesController.getPlacesByUserId);
 
 router.post("/", placesController.createPlace);
 
