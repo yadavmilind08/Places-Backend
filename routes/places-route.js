0de526eca2008f7c8032bf7d9028e71e65b9e045
Milind Yadav <yadavmilind08@gmail.com>
2021-06-01 12:18:17 +0530

    Handling errors

--- routes/places-route.js
+++ routes/places-route.js
@@ -19,13 +19,25 @@ const DUMMY_PLACES = [
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((p) => p.id === placeId);
-  
+
+  if (!place) {
+    const error = new Error("Could not find a place for provided id.");
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place });
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find((p) => p.creator === userId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for provided user id.");
+    error.code = 404;
+    return next(error);
+  }
 
   res.json({ place });
 });
