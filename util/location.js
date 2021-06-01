80057b466782be04c7648fa266dadf79c39f2c56
Milind Yadav <yadavmilind08@gmail.com>
2021-06-01 18:37:07 +0530

    Use of googles geocoding api to convert address into coordinates

---
+++ util/location.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = ""; // geocoding API key
+
+async function getCoordsForAddress(address) {
+  // dummy code as we don't have api key
+  return {
+    lat: 40.7884474,
+    lng: -73.9871516,
+  };
+
+  // code if we have api key from google geocoding api
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for specified address",
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
