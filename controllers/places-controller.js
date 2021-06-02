004ae8270c8b845615cbc9efb142574090a8704a
Milind Yadav <yadavmilind08@gmail.com>
2021-06-02 14:08:02 +0530

    Get place by place id from database

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -19,15 +19,26 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
 
-  if (!place) {
-    throw new HttpError("Could not find a place for provided id.", 404);
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not find a place.",
+      500
+    );
+    return next(error);
   }
 
-  res.json({ place });
+  if (!place) {
+    const error = new HttpError("Could not find a place for provided id.", 404);
+    return next(error);
+  }
+
+  res.json({ place: place.toObject({ getters: true }) });
 };
 
 const getPlacesByUserId = (req, res, next) => {
