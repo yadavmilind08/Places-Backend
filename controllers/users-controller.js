12ced0c5f11272ec74ff95ee1c43f90ccd2be34f
Milind Yadav <yadavmilind08@gmail.com>
2021-06-02 18:06:01 +0530

    Get all users from database

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -4,17 +4,18 @@ const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Mk",
-    email: "test@test.com",
-    password: "test",
-  },
-];
-
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password');
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching users failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+  res.json({users: users.map(user => user.toObject({ getters: true }))});
 };
 
 const signup = async (req, res, next) => {
